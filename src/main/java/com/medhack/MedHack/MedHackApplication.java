c6d819a68d37225b8423c6d615ffc3c21308a1c5
Sunimal Malkakulage <SunimalM@synergenhealth.com>
2017-09-19 08:51:00 -0500

    Initial project Structure spring-boot/gradle

---
+++ src/main/java/com/medhack/MedHack/MedHackApplication.java
@@ -0,0 +1,12 @@
+package com.medhack.MedHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedHackApplication.class, args);
+	}
+}
